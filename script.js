a90068b8577713da712e570c98ecf1293e2642c8
Lucas <lrigon2904@gmail.com>
2022-06-24 13:40:33 -0300

    finalização

--- script.js
+++ script.js
@@ -1,12 +1,25 @@
-function SnapCrackle(num){
+function prime(num){
+    for(let i = 2; i < num; i++){
+        if(num%i == 0 && num != 2){
+            return false;
+        }
+    }
+    return true
+}
+
+function SnapCrackle(number){
     let count = 1
     let str = ''
-    while(count <= num){
-        if(count%2 != 0 && count%5 != 0){
+    let i = 2
+    while(count <= number){
+        if(prime(count) == true && count != 1){
+            str += ' Prime, '
+            count++
+        }else if(count%2 != 0 && count%5 != 0){
             str += ' Snap, '
             count++
         }else if(count%2 == 0 && count%5 != 0){
-            str += count + ','
+            str += count + ', '
             count++
         }else if(count%5 == 0 && count%2 == 0){
             str += ' Crackle, '
@@ -14,9 +27,11 @@ function SnapCrackle(num){
         }else if(count%2 != 0 && count%5 == 0){
             str += ' SnapCrackle, '
             count++
-        }
+        }      
     }
-    return str
+    return str.slice(0,-2)
 }
 
-console.log(SnapCrackle(15))
+console.log(SnapCrackle(30))
+
+
